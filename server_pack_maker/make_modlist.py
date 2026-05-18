a593439b33a65545468dbb9fccc96ed2096e2cf4
milkarmelowa <emiliapliszka@onet.pl>
2026-05-18 16:15:11 +0200

    Add make_modlist.py for mod list generation

---
+++ server_pack_maker/make_modlist.py
@@ -0,0 +1,84 @@
+import zipfile
+import json
+from os import listdir
+import re
+def main():
+    # pack_chosen = choose_menu()
+    pack_chosen = '1.20.1.mrpack' # for debugging
+    result = make_lists(get_json_file(pack_chosen))
+    print(list(result.values()))
+
+def choose_menu():
+    mrpack_form = re.compile(".*\\.mrpack")
+    dir_list = listdir()
+    list_mrpack = list(filter(mrpack_form.match, dir_list))
+    if list_mrpack==[]:
+        print("No .mrpack files found")
+    else:
+        running = True
+        while running:
+            print("Detected files:")
+            for i in range (len(list_mrpack)):
+                print(str(i+1)+'.', list_mrpack[i])
+            chosen = input('Choose pack number: ')
+            while int(chosen) not in range(1, len(list_mrpack)+1):
+                chosen = input('No such pack, enter new: ')
+            pack_chosen = list_mrpack[int(chosen)-1]
+            confirm = input('Chosen pack: ' + pack_chosen + ' [Y/n]')
+            match confirm:
+                case 'Y':
+                    running = False
+                    break
+                case 'y':
+                    running = False
+                    break
+                case '':
+                    running = False
+                    break
+                case _:
+                    running = True
+                    print("cancelled\n")
+    return pack_chosen
+
+def get_json_file(pack_chosen):
+    with zipfile.ZipFile(pack_chosen) as zip_file:
+        with zip_file.open('modrinth.index.json') as json_file:
+            file = json.load(json_file)
+    return file
+
+def make_lists(file):
+    required_only_with_links = dict()
+    n = 0
+    for mod_file_json in file['files']:
+        mod_file = mod_file_json['path']
+        required_status = mod_file_json['env']['server']
+        print(mod_file,end=' -- ')
+        print(required_status)
+        if required_status == 'required' or required_status =='optional':
+            file_required = True
+        else:
+            file_required = False
+        required_only_with_links[mod_file] = file['files'][n]['downloads']
+        n+=1
+    return required_only_with_links
+
+def choose_method(result):
+    choice = ''
+    while choice != '1' and choice != '2':
+        choice = input('Make modlist locally [1]\nDownload modlist from Modrinth [2]')
+    if choice == '1':
+        make_local(list(result.keys()))
+        return 'Local complete'
+    elif choice == '2':
+        make_download(list(result.values()))
+        return 'Download complete'
+    return 'Done'
+
+def make_local(modlist):
+    return None
+
+def make_download(modlist):
+    return None
+
+if __name__ == '__main__':
+    main()
